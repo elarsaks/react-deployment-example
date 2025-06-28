9c261ed6df531f91fe84570ca4787fe7b40621df
Elar Saks <elarsaks@gmail.com>
2025-06-28 13:52:28 +0300

    Update React app itself.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p>
+          Automate
+          <span>React</span>
+          App Deployment with
+          <span>GitHub Actions</span>
+          <a
+            href="https://www.linkedin.com/pulse/automate-react-app-deployment-github-actions-elar-saks-/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn Article
+          </a>{" "}
+          <br />
+          <a
+            href="https://github.com/elarsaks/react-deployment-example"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub Repo
+          </a>
         </p>
       </div>
     );
